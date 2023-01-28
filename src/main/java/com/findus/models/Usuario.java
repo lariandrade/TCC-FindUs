c2796472127c474616173550ec71b9f48a259bf0
larissa <larissa.sandrade@hotmail.com>
2023-01-28 11:24:09 -0300

    criado tabela usuario

---
+++ src/main/java/com/findus/models/Usuario.java
@@ -0,0 +1,45 @@
+package com.findus.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Tb_Usuario")
+public class Usuario  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userID", nullable = false)
+    private Long id_Usuario;
+
+    @Column(name = "userNome", length = 50, nullable = false)
+    private String userNome;
+
+    @Column(name = "userCPF_CNPJ", length = 14, nullable = false)
+    private Integer userCPF_CNPJ;
+
+    @Column(name = "userTelefone", length = 9, nullable = false)
+    private Integer userTelefone;
+
+    @Column(name = "userEmail", length = 50, nullable = false)
+    private String userEmail;
+
+    @Column(name = "userSenha", length = 8, nullable = false)
+    private String userSenha;
+
+    @Column(name = "confirmarSenha", length = 8, nullable = false)
+    private String confirmarSenha;
+
+    @Column(name = "tipoIdentidade", length = 50, nullable = false)
+    private String tipoIdentidade;
+
+    @Column(name = "segmento", length = 50, nullable = false)
+    private String segmento;
+
+    public Usuario() {
+
+    }
+
+}
