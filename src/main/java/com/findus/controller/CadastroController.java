c737f3666864b4e60271cc8dfc1dd9505950305f
larissa <larissa.sandrade@hotmail.com>
2023-03-08 17:35:58 -0300

    ajustando css e html

---
+++ src/main/java/com/findus/controller/CadastroController.java
@@ -0,0 +1,12 @@
+package com.findus.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CadastroController {
+
+
+
+
+
+}
--- src/main/java/com/findus/controller/HomeController.java
+++ src/main/java/com/findus/controller/HomeController.java
@@ -8,9 +8,47 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController{
 
     @GetMapping("/")
-    public ModelAndView home(){
+    public ModelAndView index(){
         ModelAndView mv = new ModelAndView("index");
         return mv;
     }
+
+    @GetMapping("/login-c")
+    public ModelAndView logoc()
+    {
+        ModelAndView mv = new ModelAndView("login-c");
+        return mv;
+    }
+
+    @GetMapping("/login-p")
+    public ModelAndView logop()
+    {
+        ModelAndView mv = new ModelAndView("login-p");
+        return mv;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView registro()
+    {
+        ModelAndView mv = new ModelAndView("register");
+        return mv;
+    }
+
+
+    @GetMapping("/register-c")
+    public ModelAndView registroCliente()
+    {
+        ModelAndView mv = new ModelAndView("register-c");
+        return mv;
+    }
+
+
+    @GetMapping("/register-p")
+    public ModelAndView registroPrestador()
+    {
+        ModelAndView mv = new ModelAndView("register-p");
+        return mv;
+    }
+
 }
 
