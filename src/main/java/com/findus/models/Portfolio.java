963bfe9dfb4a95b1505006ca312c02266789a0a0
larissa <larissa.sandrade@hotmail.com>
2023-04-19 09:03:59 -0300

    renomando os id e name da pagina publicar-projeto.html para ficar igual a entidade Portfolio.

--- src/main/java/com/findus/models/Portfolio.java
+++ src/main/java/com/findus/models/Portfolio.java
@@ -15,21 +15,28 @@ public class Portfolio {
     @Column(name = "prestID", nullable = false)
     private Long prestID;
 
-    @Column(name = "portConteudo", length = 255, nullable = false, insertable=false)
-    private String portConteudo;
+    @Column(name = "portTituloPrj", length = 255, nullable = false)
+    private String portTituloPrj;
+
+    @Column(name = "portSegmento", length = 100, nullable = false)
+    private String portSegmento;
+
+    @Column(name = "portTempo", nullable = false)
+    private int portTempo;
+
+    @Column(name = "portOrcamento", length = 100, nullable = false)
+    private String portOrcamento;
 
     @Lob
     @Column(name = "portImagem", nullable = false, insertable=false)
     private byte[] portImagem;
 
-    @Column(name = "portClassificacao", length = 50)
+    @Column(name = "portClassificacao", length = 50, nullable = false)
     private int portClassificacao;
 
-    @Column(name = "portSegmento", length = 255, nullable = false)
-    private String portSegmento;
+    @Column(name = "portDescricao", length = 255, nullable = false)
+    private String portDescricao;
 
-    @Column(name = "portTituloPrj", length = 255, nullable = false)
-    private String portTituloPrj;
 
     @ManyToOne
     @JoinColumn(name = "prestador_ID")
