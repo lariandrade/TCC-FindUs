5d9dea0a8f94972d11c92fcf5bf6cc928e741cf9
larissa <larissa.sandrade@hotmail.com>
2023-05-03 13:57:42 -0300

    reestruturando pagina cadastro

--- src/main/java/com/findus/controller/CadastroController.java
+++ src/main/java/com/findus/controller/CadastroController.java
@@ -2,30 +2,29 @@ package com.findus.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CadastroController {
 
 
-    @GetMapping("/register")
-    public ModelAndView registro() {
-        ModelAndView mv = new ModelAndView("login/register");
-        return mv;
+    @GetMapping("/cliente")
+    public String tipoCliente() {
+        return "login/register-client";
     }
 
-    @GetMapping("/cadastrarCliente")
-    public ModelAndView registroCliente() {
-        ModelAndView mv = new ModelAndView("login/registro-cliente");
-        return mv;
+    @GetMapping("/prestador")
+    public String tipoPrestador() {
+        return "login/register-prest";
     }
 
-    @GetMapping("/cadastrarPrestador")
-    public ModelAndView registroPrestador() {
-        ModelAndView mv = new ModelAndView("login/registro-prestador");
-        return mv;
+    @GetMapping("/casdastroCliente")
+    public String registarCliente() {
+        return "perfil/cliente/registro-cliente";
     }
 
-
+    @GetMapping("/casdastroPrestador")
+    public String registrarPrestador() {
+        return "perfil/prestador/registro-prestador";
+    }
 }
 
