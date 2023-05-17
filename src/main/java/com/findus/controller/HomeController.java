6fdd9115c7470f5cdd49e615dcc1ab09f5b20103
larissa <larissa.sandrade@hotmail.com>
2023-05-17 13:32:43 -0300

    implementado visualização perfil do prestador - home

---
+++ src/main/java/com/findus/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.findus.controller;
+
+import com.findus.models.Portfolio;
+import com.findus.models.Prestador;
+import com.findus.repository.PortfolioRepository;
+import com.findus.service.PrestadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private PrestadorService prestadorService;
+
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+
+    @GetMapping("/visualizaPerfilPrestador")
+    public String visualizarPrestador(@RequestParam("idCliente") String idCliente, @RequestParam("idPrestador") Long idPrestador, Model model) {
+
+        Prestador prestador = prestadorService.findById(idPrestador);
+
+        List<Portfolio> projetos = portfolioRepository.findByPrestador(prestador);
+
+        model.addAttribute("emailUser", idCliente);
+        model.addAttribute("prestador", prestador);
+        model.addAttribute("projetos", projetos);
+
+
+        return "geral/visualizar-prestador";
+
+    }
+
+}
