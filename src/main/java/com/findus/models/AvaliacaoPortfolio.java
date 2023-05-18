3ed860bc788826caf0898d1765e611d8ddc27694
larissa <larissa.sandrade@hotmail.com>
2023-05-18 10:59:55 -0300

    criado entidade AvaliacaoPortfolio

---
+++ src/main/java/com/findus/models/AvaliacaoPortfolio.java
@@ -0,0 +1,24 @@
+package com.findus.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "avaliacao_portfolio")
+public class AvaliacaoPortfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "avaID", nullable = false)
+    private Long avaID;
+
+    @Column(name = "avaNota", length = 10, nullable = false)
+    private Integer avaNota;
+
+    @Column(name = "avaIdProjeto", length = 10, nullable = false)
+    private Integer avaIdProjeto;
+
+}
