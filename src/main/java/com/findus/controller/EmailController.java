79e20d8578247e188bee2b526ab2eb696156ce4f
larissa <larissa.sandrade@hotmail.com>
2023-05-18 15:03:33 -0300

    ajustes email

--- src/main/java/com/findus/controller/EmailController.java
+++ src/main/java/com/findus/controller/EmailController.java
@@ -21,12 +21,34 @@ public class EmailController {
         return "geral/contato";
     }
 
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @PostMapping("/enviar-email")
     public void enviarEmail(@RequestParam("destinatario") String destinatario, @RequestParam("corpo") String corpo) throws MessagingException {
 
-        String assunto = "Solicitação de Serviço.";
+        String assunto = "Você tem uma nova solicitação de serviço.";
 
-        emailService.enviarEmail(destinatario, assunto, corpo);
+
+
+        String mensagem = "<html><head>"
+                + "<style>"
+                + "body { font-family: Arial, sans-serif; }"
+                + "h1 { color: #333333; }"
+                + "p { color: #666666; }"
+                + ".button { background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }"
+                + "</style>"
+                + "</head><body>"
+                + "<h1>Olá!</h1>"
+                + "<p>Este é um e-mail de teste com estilos CSS.</p>"
+                + "<p>Clique no botão abaixo:</p>"
+                + "<a href=\"http://localhost:8090/\"><button class=\"button\">Visualizar Cliente</button></a>"
+                + corpo + "</body></html>";
+
+
+        emailService.enviarEmail(destinatario, assunto, mensagem);
+
     }
 
 }
--- src/main/java/com/findus/service/EmailService.java
+++ src/main/java/com/findus/service/EmailService.java
@@ -3,8 +3,10 @@ package com.findus.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,16 +14,24 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+    private final String emailUsername;
 
-    public void enviarEmail(String destinatario, String assunto, String corpo) throws MessagingException {
+    public EmailService(JavaMailSender javaMailSender, @Value("${spring.mail.username}") String emailUsername) {
+        this.javaMailSender = javaMailSender;
+        this.emailUsername = emailUsername;
+    }
+
+    @Async
+    public void enviarEmail(String destinatario, String assunto, String mensagem) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        helper.setFrom("contato.findus@hotmail.com"); //precisa definir pois está dando erro de acesso
+        helper.setFrom(emailUsername);
         helper.setTo(destinatario);
         helper.setSubject(assunto);
-        helper.setText(corpo);
+        helper.setText(mensagem, true); // Defina como true para permitir HTML no corpo do e-mail
 
         javaMailSender.send(message);
     }
 }
+
