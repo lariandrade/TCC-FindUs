97b182b9e897de18273cde82806978f149a7cafe
larissa <larissa.sandrade@hotmail.com>
2023-05-21 13:13:10 -0300

    implementado denuncia

--- src/main/java/com/findus/controller/DenunciaController.java
+++ src/main/java/com/findus/controller/DenunciaController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,10 +26,16 @@ public class DenunciaController {
     @Autowired
     private PrestadorService prestadorService;
 
-    @GetMapping("/denunciarPrestador/{id}")
-    public String denunciarPrestador(@PathVariable("id") Long idCliente, Model model) {
+    @GetMapping("/denunciarPrestador")
+    public String denunciarPrestador(@RequestParam("idprestador") Long idPrestador, @RequestParam("idcliente") Long idCliente, Model model) {
 
-        return "geral/denunciar";
+        Prestador usuario = prestadorService.findById(idPrestador);
+        Cliente denunciado = clienteService.findById(idCliente);
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("denunciado", denunciado);
+
+        return "geral/denunciarPrestador";
 
     }
 
@@ -80,6 +85,40 @@ public class DenunciaController {
         denunciaService.enviarDenuncia(mensagemFormatada, idUsuario, idDenunciado, assunto, motivoDenuncia);
 
         return "redirect:/visualizaPerfilPrestador?idCliente=" + cliente.getUserEmail() + "&idPrestador=" + idDenunciado;
+
+    }
+
+    @PostMapping("/enviar-denuncia-prestador")
+    public String enviarDenunciaPrestador(@RequestParam("corpo") String motivoDenuncia,
+                                        @RequestParam("idUsuario") Long idUsuario,
+                                        @RequestParam("idDenunciado") Long idDenunciado) throws MessagingException {
+
+
+        String assunto = "Nova denuncia.";
+
+        Cliente cliente = clienteService.findById(idDenunciado);
+
+        String mensagemFormatada = "<html><head>"
+                + "<style>"
+                + "body { font-family: Arial, sans-serif; }"
+                + "h3 { color: #333333; }"
+                + "p { color: #666666; }"
+                + ".button { background-color: #000; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }"
+                + "</style>"
+                + "</head><body>"
+                + "<h3>Informações do cliente denunciado:</h3>"
+                + "<br>"
+                + "<p>Nome completo: " + cliente.getUserNome() + "</p>"
+                + "<p>CPF/CNPJ: " + cliente.getUserCPF_CNPJ() + "</p>"
+                + "<p>Email: " + cliente.getUserEmail() + "</p>"
+                + "<br>"
+                + "<h3>Reclamação reportada:</h3>"
+                + "<p>"+motivoDenuncia+"</p>"
+                + "</body></html>";
+
+        denunciaService.enviarDenuncia(mensagemFormatada, idUsuario, idDenunciado, assunto, motivoDenuncia);
+
+        return "redirect:/visualizarCliente?idCliente=" + idDenunciado + "&idPrestador=" + idUsuario;
 
     }
 
