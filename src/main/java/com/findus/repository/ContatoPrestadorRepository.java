6b2ed22fed0e780b5e92bd5a82cd82476aa9e679
larissa <larissa.sandrade@hotmail.com>
2023-06-10 13:40:08 -0300

    fix/corrigindo avaliacao

--- src/main/java/com/findus/repository/ContatoPrestadorRepository.java
+++ src/main/java/com/findus/repository/ContatoPrestadorRepository.java
@@ -4,7 +4,9 @@ import com.findus.models.ContatoPrestador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContatoPrestadorRepository extends JpaRepository<ContatoPrestador,Long> {
-
+    List<ContatoPrestador> findByContIdPrestador(Long idPrestador);
 }
